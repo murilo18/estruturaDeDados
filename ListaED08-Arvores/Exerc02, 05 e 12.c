02949b5b1ae7a6f9b070e6ee0ceff3247b247ac7
Murilo <murilo980ti@outlook.com>
2017-12-04 17:13:06 -0200

    Questões 2, 5 e 12

---
+++ ListaED08-Arvores/Exerc02, 05 e 12.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	int numero;
+	struct arvore *esquerdo, *direito;
+}arvore;
+
+arvore* criar(arvore *raiz, int dado){
+	if(raiz == NULL){
+		raiz = (arvore*)malloc(sizeof(arvore));
+		raiz->numero = dado;
+		raiz->esquerdo = NULL;
+		raiz->direito = NULL;
+	}else if(dado > raiz->numero){
+		raiz->direito = criar(raiz->direito, dado);
+	}else{
+		raiz->esquerdo = criar(raiz->esquerdo, dado);
+	}
+	return raiz;
+}
+
+void mostrar(arvore *node){
+	if(node != NULL){
+		printf("%i ", node->numero);		
+		mostrar(node->direito);
+		mostrar(node->esquerdo);	
+	}
+}
+
+//2ª - Uma funcao que conta o número de nós de uma árvore binária
+int tamanho(arvore *raiz) { 
+	//Sintaxe: Condição ? verdadeiro : falso
+    return (raiz != NULL) ? (tamanho(raiz->esquerdo) + tamanho(raiz->direito) + 1) : 0;
+    //se o nó atual for diferente de NULL acontecerá a contagem de nós, senão, somará zero 
+}
+
+//5ª Uma funcao que calcula a altura de uma árvore binária.
+int altura(arvore *raiz){
+	if (raiz == NULL) 
+      return -1;
+   else {
+   		int hd = altura(raiz->direito);
+    	int he = altura(raiz->esquerdo);
+      if (he < hd) return hd + 1;
+      else return he + 1;
+   }
+}
+
+//12ª funcao que troca a subárvore direita pela subárvore esquerda de todos os nós da árvore
+void espelho(arvore *raiz){
+	if(raiz != NULL){
+		arvore *aux;
+		aux = raiz->esquerdo;
+		raiz->esquerdo = raiz->direito;
+		raiz->direito = aux;
+		espelho(raiz->direito);
+		espelho(raiz->esquerdo);
+	}
+}
+
+int main(){
+	arvore *teste = NULL;
+	
+	teste = criar(teste, 10);
+	teste = criar(teste, 2);
+	teste = criar(teste, 1);
+	teste = criar(teste, 5);
+	teste = criar(teste, 4);
+	teste = criar(teste, 6);
+	teste = criar(teste, 11);
+	
+	printf("Tamanho: %i\n", tamanho(teste));
+	printf("Altura: %i\n", altura(teste));
+	printf("Resultado: ");
+	mostrar(teste);
+	espelho(teste);
+	printf("\nResultado trocado: ");
+	mostrar(teste);
+	return 0;
+}
